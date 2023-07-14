3881419a55a6f833c0d5b6e9398965fdd8b75e5d
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-14 10:14:41 +0800

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,16 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <div>
-      <div className="section">
+      <div className="section"></div>
+      <div className="box">
+        <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+          <color attach="background" args={["#ececec"]} />
+          <Experience />
+        </Canvas>
         <h1 className="section-middle">
           site maker:DennisLee-pcleegood@gmail.com
         </h1>
       </div>
-      <div className="box">
-        <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-          <color attach="background" args={["#ececec"]} />
-          <Experience />s
-        </Canvas>
-      </div>
-      <Footer />
     </div>
   );
 }
